cb009c7ff3752ef021fd0e303c727d619da2076a
Présentation <tbr@tbr.com>
2023-06-20 12:26:51 +0200

    * microservice impl - refactor conf

--- advanced-java-parent/adv-java-micro-service/src/main/java/fr/epita/web/app/TestRestControllers.java
+++ advanced-java-parent/adv-java-micro-service/src/main/java/fr/epita/web/app/TestRestControllers.java
@@ -1,11 +1,19 @@
 package fr.epita.web.app;
 
 
+
+import fr.epita.advjava.datamodel.Country;
 import fr.epita.advjava.services.CountryDAO;
+import fr.epita.web.resources.CountryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 public class TestRestControllers {
@@ -18,6 +26,18 @@ public class TestRestControllers {
     @GetMapping(path = "/someContent")
     public ResponseEntity<String> firstGetMethod(){
         return ResponseEntity.ok("test sucessful");
+    }
+
+    @PostMapping(value = "/countries",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<String> firstPostMethod(@RequestBody CountryDTO dto){
+        Country country = new Country(dto.getCode(), dto.getDisplayName());
+        dao.create(country);
+        return ResponseEntity
+                .created(URI.create("/countries/" + country.getShortCode()))
+                .body("created the country sucessfully");
     }
 
 }
